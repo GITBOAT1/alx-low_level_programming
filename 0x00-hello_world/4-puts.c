d741925cf138e28f5f4a8a449ffcc572ff7d832e
joe <boat232001@yahoo.com>
2021-08-26 20:49:42 -0700

    Hello, printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 /**
-  * The main fuction control the flow state.
-  * It return a vlue of 0 to the program since it's int
+ *
+ * Description: This is a longer description for main.
+ * Don't forget that a line should not exceed 80 characters.
+ * But you're totally free to use several lines to properly
+ * describe your function
+ * Return: The main return a 0 for succes
  */
 int main(void)/*print to screen*/
 {
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Description: main C program that prints exactly with proper grammar,
+ * but the outcome is a piece of art and return 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
