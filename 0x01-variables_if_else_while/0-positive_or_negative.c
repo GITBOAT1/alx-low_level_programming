1ec1b92cca74d2094afef9370c7c6f00d1cdc36c
joe <boat232001@yahoo.com>
2021-08-27 05:07:27 -0700

    if statement

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main controls the flow of the program
+ * @void: this is an empty variable
+ * Return: this confirm the success of opration
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+		printf("%d is positive", n);
+	else if (n == 0)
+		printf("%d is zero", n);
+	else
+		printf("%d is negative", n);
+	printf("\n")
+	return (0);
+}
