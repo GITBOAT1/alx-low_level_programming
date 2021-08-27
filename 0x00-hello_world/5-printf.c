cae3545cd34780987fa7f42ad78a03caf11db068
joe <boat232001@yahoo.com>
2021-08-26 21:07:15 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 /**
- * Description: main C program that prints exactly with proper grammar,
- * but the outcome is a piece of art and return 0
- */
+ * function_name - Short description, single line
+ * @parameterx: Description of parameter x
+(* a blank line
+* Description: Longer description of the function)?
+(* section header: Section description)*
+* Return: returned 0 value meaning a sucess
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
