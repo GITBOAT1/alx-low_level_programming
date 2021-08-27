67b8dfdd33651425c02bea2843497b022c8dc224
joe <boat232001@yahoo.com>
2021-08-26 21:22:29 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
  *
- * Description: This is a longer description for main.
- * Don't forget that a line should not exceed 80 characters.
- * But you're totally free to use several lines to properly
- * describe your function
+ * main - This is a longer description for main.
+ * @void: this is a default
+ *
  * Return: The main return a 0 for succes
  */
-int main(void)/*print to screen*/
+
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzlz");
 	return (0);
