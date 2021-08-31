21bb058d606b879fc019235eea1c89a83c64496e
joe <boat232001@yahoo.com>
2021-08-31 09:35:38 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - This is where the program begins
+ * @void: This shows its empty
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int main(void)
+{
+	printf("Holberton\n");
+	return (0);
+}
