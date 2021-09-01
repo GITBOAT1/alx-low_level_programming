9d612e9c536ae2ff5491a0f569671ab43b934065
joe <boat232001@yahoo.com>
2021-08-31 18:35:30 -0700

    _pushchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print the alphabet
+ * @ch++: just incremet on every loop
+ * Return: 0 means sucess
+ */
+
+int print_alphabet(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+	return (0);
+}
