4c0f85f0f9fa79e5d3712d20c015c2009eb32f74
joe <boat232001@yahoo.com>
2021-08-31 18:58:24 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print the alphabet 10x in a nested loop
+ * @ch++: just incremet on every loop
+ * Return: 0 means sucess
+ */
+
+int print_alphabet(void)
+{
+	char ch;
+	int  ch1;
+
+	for (ch1 = 1; ch1 <= 10; ch1++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
