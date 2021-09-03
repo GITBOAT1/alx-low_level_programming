56b3d0a42d434279bb1f855fa4fe175cdb21a489
joe <boat232001@yahoo.com>
2021-09-02 22:28:49 -0700

    add me

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+/**
+ * add - simply adds the numbers
+ * @n1: just incremet on every loop
+ * @n : supply for the function
+ *Return: the anser  sucess
+ */
+
+int add(int n, int n1)
+{
+	(return n + n1);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -19,3 +19,4 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 void printnumber(int);
+int add(int, int);
