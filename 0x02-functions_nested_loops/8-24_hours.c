222fe86b65a3c7408291bc26ca86b10aaed1ab79
joe <boat232001@yahoo.com>
2021-09-02 21:28:42 -0700

    timtable and jack

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- * void jack_bauer - print the alphabet 10x in a nested loop
+ * jack_bauer - print the alphabet 10x in a nested loop
+ * printnumber - It purpose is to only print out num
  * @se++: just incremet on every loop
  * ++: just incremet on every loop
- * @hr++: just incremet on every loop 
+ * @hr++: just incremet on every loop.
  * Return: 0 means sucess
  */
 void jack_bauer(void)
