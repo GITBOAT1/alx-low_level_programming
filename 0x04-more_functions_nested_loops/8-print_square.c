975ebc2b8a07da7e121397d3a067db252746e2d7
joe <boat232001@yahoo.com>
2021-09-04 01:30:59 -0700

    spure

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_square - print line out number form 0-9
+ * @n: supplied from the main to.
+ * Return: Always 0.
+ */
+void print_square(int size)
+{
+
+	int a, b;
+
+	for (a = 0; a < size; a++)
+	{
+		for (b = 0; b < size; b++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+
+}
