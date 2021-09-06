90443e73e1bf9d4cd271703ae79b8d42c4a9f138
joe <boat232001@yahoo.com>
2021-09-05 19:35:40 -0700

    start of a pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - this chage the value of a given n
+ *
+ * @n: this valude holds the pointer
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+# The start of ALMIGHT POINTER #
