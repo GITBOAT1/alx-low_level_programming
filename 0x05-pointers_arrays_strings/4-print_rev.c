4be7ab83ea456b9b03591296b270e15b5fdca61a
joe <boat232001@yahoo.com>
2021-09-06 12:38:55 -0700

    the reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - check the code
+ * @s: the is the suppled words to be reversed
+ * Return: Always 0.
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i = i + 1;
+	}
+	while (i > 0)
+	{
+		_putchar(s[i]);
+
+		i = i - 1;
+	}
+}
