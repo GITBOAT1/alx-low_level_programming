c831042d8cb96c0f6c05fe35da8267f22fcb6bcc
joe <boat232001@yahoo.com>
2021-09-06 12:25:25 -0700

    the puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _put - check the code
+ * @str: the hold a long string
+ * Return: Always 0.
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i = i + 1;
+		if (str[i] == '\n')
+			break;
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,6 +3,8 @@
 
 #include <stdio.h>
 
+int _putchar(char c);
+void _puts(char *str);
 int _strlen(char *s);
 void swap_int(int *a, int *b);
 void reset_to_98(int *n);
