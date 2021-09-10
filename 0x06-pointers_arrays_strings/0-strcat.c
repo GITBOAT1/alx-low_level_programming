773d115cb5f201275d054f68e5b6a4baabfe5be5
joe <boat232001@yahoo.com>
2021-09-10 10:25:11 -0700

    update from pld

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,23 +10,29 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	char *ptr;
+	int len1, len2, i;
+	char *ptr, *ptr2;
 
-	while (*(++dest))
+	len1 = 0, len2 = 0, i = 0;
+	while (*(dest + len1) != '\0')
 	{
 		/**
 		 * move until the end of line
 		 */
+		len1++;
 	}
 
-	while (*src)
+	while (*(src + len2) != '\0')
 	{
-		*dest = *src;
-		dest++;
-		src++;
+		len2++;
 	}
-	*dest = '\0';
+	ptr = &dest[len1];
+	ptr2 = src;
 
-	ptr = dest;
-	return (ptr);
+	for (i = 0; i < len2; i++)
+	{
+		*(ptr + i) = *(ptr2 + i);
+	}
+
+	return (dest);
 }
