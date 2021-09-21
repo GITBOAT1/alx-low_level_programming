31910bd6487cc00d1a6829ec9d80e07276702d60
joe <boat232001@yahoo.com>
2021-09-21 14:28:41 -0700

    hello

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,7 +15,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	len = (width * height);
+	len = ((sizeof(int) * width) * (sizeof(int) * height));
 	arr = (int **)malloc(len);
 	if (arr == NULL)
 		return (NULL);
