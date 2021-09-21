a14f62af7d03863d52478585712e0363b4a3b2f0
joe <boat232001@yahoo.com>
2021-09-21 12:09:50 -0700

    hey there

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - prints buffer in hexa
+ * @str: the address of memory to print
+ * for chaecker sake  the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+
+char *_strdup(char *str)
+{
+	int i, length;
+	char *mem1;
+
+
+	i = 0, length = 0;
+	if (!str)
+	{
+		return (NULL);
+	}
+
+	while (str[length])
+	{
+		length += 1;
+	}
+
+	mem1 = malloc(length);
+	{
+		while (str[i] != '\0')
+		{
+			mem1[i] = str[i];
+			i = i + 1;
+		}
+	}
+	return (mem1);
+}
