ca8bac1e641b8d516ac0b994efa239b266f732a6
joe <boat232001@yahoo.com>
2021-09-27 15:08:32 -0700

    macros

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define ABS(a) ((a <=-1) ? \
+	(a = (a * (-1))):   \
+	(a >= 0) ? (a))
+#endif
