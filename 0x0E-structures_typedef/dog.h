0a58fc8c2d9d2d427894e0256ca6e0d55757c58c
joe <boat232001@yahoo.com>
2021-09-27 11:26:37 -0700

    The dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+#endif
