208a7712625dca2c2e2f52a150bdde85d6d63e70
joe <boat232001@yahoo.com>
2021-09-30 12:19:58 -0700

    2 up

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,12 +16,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 	count = 0;
 	if (size <= 0)
 		return (-1);
-	for (i = 0; i < size; i++)
+	if (cmp && array)
 	{
-		if (cmp(array[i]) > 0)
-			count = count + 1;
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) > 0)
+				count = count + 1;
+		}
 	}
-
 	if (count <= 0)
 		return (-1);
 	return (count);
