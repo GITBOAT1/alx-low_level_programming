d1c3479690bd82e644b743e8f8e285aac4c707fd
joe <boat232001@yahoo.com>
2021-10-07 23:31:40 -0700

    adding the a node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * add_node - create a node and initialize it
+ * @head:The node to start from
+ * @str: the string to store to the node
+ * Return: the created node.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
+	new_node->next = *head;
+	*head = new_node;
+	return (*head);
+}
