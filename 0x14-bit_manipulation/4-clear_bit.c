7182b58f047396bd0aaa2cdc65759d763fee60fb
joe <boat232001@yahoo.com>
2021-10-15 03:30:32 -0700

    clear me

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,17 +1,17 @@
 #include  "main.h"
 
 /**
- * set_bit - set a certain number to an index provided
+ * clear_bit - set a certain bits 0
  * @n: This is the number for search
  * @index: what position to search
  * Return: Always 1 or zero.
  */
 
-int set_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	int l;
 
-	l = (*n |= 1UL << index);
+	l = (*n &= ~(1UL << index));
 
 	if (l == '\0')
 	{
