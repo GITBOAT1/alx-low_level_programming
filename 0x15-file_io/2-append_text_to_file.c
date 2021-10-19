73dbd2a8e5304eb6846412634e60a6e5d4b02902
joe <boat232001@yahoo.com>
2021-10-18 19:40:07 -0700

    append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * create_file - check the code
+ * append_text_to_file - check the code
  * @filename: the name to use
  * @text_content: where to save
  * Return: Always 0.
  */
-int int append_text_to_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd = open(filename, O_APPEND, O_WRONLY, 0600), i;
 
