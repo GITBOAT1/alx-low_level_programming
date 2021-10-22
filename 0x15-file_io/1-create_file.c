9c2fa9c0c067283cce295f3f67fc8515593ddce6
joe <boat232001@yahoo.com>
2021-10-22 00:28:55 -0700

    up

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,18 +7,23 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd = open(filename, O_CREAT | O_APPEND | O_WRONLY, 0600), i;
+	int fd = open(filename, O_CREAT |O_TRUNC | O_WRONLY, 0600), i;
 	int l = 0;
 
 	if (fd == -1)
 		return (fd);
 	if (text_content == NULL)
+	{
+		close(fd);
+		return (-1);
+	}
+	if (filename == NULL)
 		return (-1);
 	while (text_content[l] != '\0')
 	{
 		l = l + 1;
 	}
-	printf("size of char %d\n", l);
+;
 	i = write(fd, text_content, l + 1);
 	close(fd);
 
