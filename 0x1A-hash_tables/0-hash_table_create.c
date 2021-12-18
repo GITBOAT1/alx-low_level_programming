62e37fd2b43d02d944845d4713b9f99e09a74057
GITBOAT <boat232001@yahoo.com>
2021-12-18 08:31:14 -0800

    test

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,6 +12,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	hash_table_t *ht = malloc(sizeof(hash_table_t));
 
+	if (!ht)
+	{
+		return (NULL);
+	}
 	ht->array = malloc(sizeof(size) * size);
 	return (ht);
 }
