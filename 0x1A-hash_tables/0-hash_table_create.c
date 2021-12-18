c1377bbe63fb39d5ed280f813dd36fea7c4e1dcd
GITBOAT <boat232001@yahoo.com>
2021-12-18 07:52:30 -0800

    test

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,13 +2,16 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_creat - this is the part that allocte the memory
+ * hash_table_create - this is the part that allocte the memory
  * @size: the amount to locate.
+ * Return: the memory
 */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *ht = malloc(sizeof(hash_table_t));
-    ht->array = malloc(sizeof(size) * size);
-    return (ht);
+
+	hash_table_t *ht = malloc(sizeof(hash_table_t));
+
+	ht->array = malloc(sizeof(size) * size);
+	return (ht);
 }
