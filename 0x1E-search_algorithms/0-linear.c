c67777e2f79cf25a954643c2e6520c323d7c64cd
GITBOAT <boat232001@yahoo.com>
2022-04-02 17:43:28 -0700

    02

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -3,8 +3,10 @@
 #include "search_algos.h"
 
 /**
- * main - Entry point
- *
+ * linear_search - Entry point
+ * @array:- Where array is a pointer to the first element of the array to search in
+ * @size:- is the number of elements in array
+ * @value:-  is the value to search for
  * Return: Always EXIT_SUCCESS
  */
 
@@ -16,9 +18,9 @@ int linear_search(int *array, size_t size, int value)
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (value == array[i])
 	  	{	
-			return(array[i]);
+			return(i);
 		}
-		else if ( i == size)
+		else if ( i == size-1)
 		{
 			return(-1);
 		}
