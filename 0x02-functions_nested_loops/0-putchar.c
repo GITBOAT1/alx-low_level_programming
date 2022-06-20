932d08df8fb6f91571b43aebb7872fc8b558aab4
GITBOAT <boat232001@yahoo.com>
2022-06-20 08:51:45 -0700

    updates

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * main - This is where the program begins
+ * @void: This shows its empty
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 int main(void)
 {
-	printf("Holberton\n");
+	printf("_putchar\n");
 	return (0);
 }
