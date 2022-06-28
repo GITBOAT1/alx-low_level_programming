27f2554e319eb6f62d0918ae2a7432725d2b28b9
GITBOAT <boat232001@yahoo.com>
2022-06-28 08:43:52 -0700

    debug corrections

--- 0x03-debugging/3-main_a.c
+++ 0x03-debugging/3-main_a.c
@@ -20,8 +20,31 @@ int main(void)
     printf("Date: %02d/%02d/%04d\n", month, day, year);
 
     day = convert_day(month, day);
+	print_remaining_days(month, day, year);
+	
+	printf("====================\n\n");
+	convert_day(4,10);
+	print_remaining_days(4,10,1997);
 
-    print_remaining_days(month, day, year);
+	printf("====================\n\n");
+	convert_day(2,12);
+	print_remaining_days(2,12,2019);
+
+	printf("====================\n\n");
+	convert_day(2,29);
+	print_remaining_days(2,29,2000);
+
+	printf("====================\n\n");
+	convert_day(3, 1);
+	print_remaining_days(3,1,2009);
+
+	printf("====================\n\n");
+	convert_day(3,1);
+	print_remaining_days(3,1,2008);
+
+	printf("====================\n\n");
+	convert_day(4,10);
+	print_remaining_days(4,10,4000);
 
     return (0);
 }
