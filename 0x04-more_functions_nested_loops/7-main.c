c1958debc70a080ec5f1dfeff37378740d49f996
GITBOAT <boat232001@yahoo.com>
2022-06-28 10:10:22 -0700

    corrections

--- 0x04-more_functions_nested_loops/7-main.c
+++ 0x04-more_functions_nested_loops/7-main.c
@@ -7,13 +7,9 @@
  */
 int main(void)
 {
-    print_diagonal(0);
-    print_diagonal(2);
-    print_diagonal(10);
-    print_diagonal(-4);
-	printf("\n============3\n");
+
 	print_diagonal(3);
-	printf("\n===========9\n");
+
 	print_diagonal(9);
     return (0);
 }
