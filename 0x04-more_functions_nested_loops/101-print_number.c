94ba1fd978a8a28d5547d27228732c70f8daecd9
GITBOAT <boat232001@yahoo.com>
2022-07-01 03:32:15 -0700

    101-print

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * print_number- prints an integer using putchar
+ * @n : supplied number to pring
+ * Return : nothing
+*/
+
+void print_number(int n)
+{
+	int c;
+	int i;
+	int x;
+	unsigned int y;
+	unsigned int z;
+
+	i = 1;
+	y = n;
+	z = n;
+	c = 0;
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		y = y + 1;
+		y = y * -1;
+		y = y + 1;
+		z = z + 1;
+		z = z * -1;
+		z = z + 1;
+	}
+	while (y > 0)
+	{
+		y = y / 10;
+		c++;
+	}
+	for (x = 1; x < c; x++)
+	{
+		i = i * 10;
+	}
+	for (x = 0; x < c; x++)
+	{
+		_putchar(z / i + '0');
+		z = z % i;
+		i = i / 10;
+	}
+
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -14,7 +14,7 @@ int mul(int, int);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
-
+void print_number(int n);
 #endif
 /**
  * MAIN_H
