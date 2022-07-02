6de3498911e4e385943316566cda30bbffc4556a
GITBOAT <boat232001@yahoo.com>
2022-07-02 11:24:32 -0700

    pointers update

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,17 +11,19 @@ void print_rev(char *s)
 	int i;
 
 	i = 0;
-
 	while (s[i] != '\0')
 	{
 		i = i + 1;
 	}
 
-	while (i != -1)
+	if ( i > 0)
 	{
-		_putchar(s[i]);
-
-		i--;
-	}
+		while (i != -1)
+		{			_putchar(s[i]);
+			i--;
+		}	
+	
 	_putchar('\n');
+	}
+
 }
