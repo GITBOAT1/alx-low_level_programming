f282d3693afd2c6d8c5730721dd44a3785f4ad97
GITBOAT <boat232001@yahoo.com>
2022-07-03 13:39:27 -0700

    reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * print_rev - check the code
+ * @s: the is the suppled words to be reversed
+ * Return: Always 0.
+ */
+
+void rev_string(char *s)
+{
+	size_t len =  strlen(s), i = 0, j = len - 1;
+	char *tmp = malloc(len + 1);
+
+
+	strcpy(tmp, s);
+	for (i = 0; i < len; i++)
+	{
+		s[i] = tmp[j];
+		j--;
+	}
+
+	free(tmp);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -9,7 +9,7 @@ void _puts(char *str);
 int _strlen(char *s);
 void swap_int(int *a, int *b);
 void reset_to_98(int *n);
-
+void rev_string(char *s);
 #endif
 /**
  * MAIN_H
