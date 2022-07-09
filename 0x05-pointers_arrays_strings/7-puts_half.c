c5f228ef063b9ee8bdfd5067fd663a3a2ba02ff9
GITBOAT <boat232001@yahoo.com>
2022-07-08 23:45:54 -0700

    updates

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * puts_half - function that prints half of a string.
+ * @str: value for printing
+ * Retrun 0;
+*/
+
+void puts_half(char *str)
+{
+	int i, j;
+
+
+	i = strlen(str);
+	if (i % 2 == 0)
+	{
+		i = i / 2;
+		for (j = 0; j < i; j++)
+		{
+			printf("%c", str[j + i]);
+		}
+	}
+	else
+	{
+		printf("%c", str[strlen(str) - 1]);
+	}
+	printf("\n");
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -11,7 +11,7 @@ void swap_int(int *a, int *b);
 void reset_to_98(int *n);
 void rev_string(char *s);
 void puts2(char *str);
-
+void puts_half(char *str);
 #endif
 /**
  * MAIN_H
