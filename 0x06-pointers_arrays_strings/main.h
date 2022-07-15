28a734b30292cceecbacc963706a19b18f628dde
GITBOAT <boat232001@yahoo.com>
2022-07-15 07:50:02 -0700

    update

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,4 +10,7 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 int _putchar(char c);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
 #endif
