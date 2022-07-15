22c5eb108ac8045e1940fc6289b3608375fb3b15
GITBOAT <boat232001@yahoo.com>
2022-07-15 08:16:44 -0700

    update

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rot13 - encodes using a as 13
+ * @s: char pointer
+ * rot13: encodes into leet speak
+ * Return: char pointer s
+ */
+char *rot13(char *s)
+{
+
+	int i;
+	int j;
+	char a[] = {'A', 'N', 'B', 'O', 'C', 'P', 'D', 'Q', 'E', 'R',
+		    'F', 'S', 'G', 'T', 'H', 'U', 'I', 'V', 'J', 'W',
+		    'K', 'X', 'L', 'Y', 'M', 'Z', 'N', 'A', 'O', 'B',
+		    'P', 'C', 'Q', 'D', 'R', 'E', 'S', 'F', 'T', 'G',
+		    'U', 'H', 'V', 'I', 'W', 'J', 'X', 'K', 'Y', 'L',
+		    'Z', 'M', 'a', 'n', 'b', 'o', 'c', 'p', 'd', 'q',
+		    'e', 'r', 'f', 's', 'g', 't', 'h', 'u', 'i', 'v',
+		    'j', 'w', 'k', 'x', 'l', 'y', 'm', 'z', 'n', 'a',
+		    'o', 'b', 'p', 'c', 'q', 'd', 'r', 'e', 's', 'f',
+		    't', 'g', 'u', 'h', 'v', 'i', 'w', 'j', 'x', 'k',
+		    'y', 'l', 'z', 'm'};
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (a[j] != '\0')
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = a[j + 1];
+				break;
+			}
+			j += 2;
+		}
+		i++;
+	}
+	return (s);
+}
